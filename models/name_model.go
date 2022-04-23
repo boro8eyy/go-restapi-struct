7264f5cd781b2fc858018a886b8f2c90bd045c62
boro8eyy <bogdanhohlov1976@gmail.com>
2022-04-23 07:15:52 +0300

    some fixes

--- models/name_model.go
+++
@@ -1,7 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type NameModel struct {
-	Id primitive.ObjectID `bson:"id,omitempty"`
-}
--- responses/name_response.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/gofiber/fiber/v2"
-
-type NameResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
-	Payload    *fiber.Map `json:"payload"`
-}
